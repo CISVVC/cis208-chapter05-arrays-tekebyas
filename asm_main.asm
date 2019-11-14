1b97877d686c12082c29006f878bb9f8a1ccacb0
teke byas <tekeb6687@student.vvc.edu>
2019-11-13 20:06:26 -0800

    array used in loop, data updates

--- asm_main.asm
+++ asm_main.asm
@@ -12,6 +12,11 @@ segment .data
         SUCCESS: equ 0
         kernelcall: equ 80h
 
+        arr     db  1,2,3,4,5           ; declares an array with 5 positions containing
+                                        ; the range 1 to 5
+        size    db  5
+        scalar  db  4
+
 
 
 ; uninitialized data is put in the .bss segment
@@ -28,6 +33,28 @@ asm_main:
         pusha
 ; *********** Start  Assignment Code *******************
 
+        xor     eax, eax
+        mov     eax, arr            ; moves the address of the first position 
+                                    ; of the array to eax
+        xor     ebx, ebx
+        mov     ebx, 5              ; move the size of the array to ebx
+
+        xor     ecx, ecx
+        mov     ecx, 4              ; ecx holds scalar
+
+        push    eax
+        push    ebx
+        push    ecx
+
+        call    scale
+
+        add     esp, 12             ; clear used data off of stack
+
+        mov     eax, [arr]
+        call    print_int
+        call    print_nl
+
+
 ; *********** End Assignment Code **********************
 
         popa
@@ -35,4 +62,24 @@ asm_main:
         leave                     
         ret
 
+
+scale:
+        push    ebp
+        mov     ebp, esp
+
+        mov     ecx, [ebp+12]               ; stores size in ecx for loop
+        mov     ebx, [ebp+8]                ; stores scalar in ebx
+        mov     esi, [ebp+16]               ; store first positon of array in esi
+                                            ; what is stored in eax is the address of the array
+                                            ; as opposed to moving the entire array
+
+        loop_start:
+            mov     eax, [esi + ecx]        ; moves the contents of the beginning of the
+                                            ; array offset by what position we are at
+                                            ; into eax for multiplication
+            mul     ebx                     ; multiplies what is in eax by bx
+            loop    loop_start
+
+        pop     ebp
+        ret
 
